0d3c260afb1d472beb72b9913f23184222473a73
Puma <Pumaredblue@gmail.com>
2022-10-23 19:41:49 +0300

    1. Make interface

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var redSignal: UIView!
+    @IBOutlet weak var yellowSignal: UIView!
+    @IBOutlet weak var greenSignal: UIView!
+
+    
+    @IBOutlet weak var switchButtonStyle: UIButton!
+    
+    
+    func makeCircle(view: UIView) {
+//        view.contentMode = UIView.ContentMode.scaleToFill
+        view.layer.cornerRadius = view.frame.height / 2
+        print(view.frame.height)
+        print(view.frame.size.height / 2)
+        view.layer.masksToBounds = true
+//        view.clipsToBounds = true
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        makeCircle(view: redSignal)
+        makeCircle(view: yellowSignal)
+        makeCircle(view: greenSignal)
     }
 
-
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        switchButtonStyle.configuration = setupButton(with: "Start")
+        redSignal.alpha = 0.5
+        yellowSignal.alpha = 0.5
+        greenSignal.alpha = 0.5
+    }
+    
+    
+    @IBAction func switchButton() {
+        switchButtonStyle.configuration = setupButton(with: "Next")
+    
+        
+    }
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.title = title
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+        return buttonConfiguration
+    }
 }
 
